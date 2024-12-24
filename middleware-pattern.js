870d2081e70067778d1d8ab7d9e8c121399e0ace
chethankumar123 <chethananderson4@gmail.com>
2024-12-24 21:01:53 +0530

    middleware-pattern.js

---
+++ middleware-pattern.js
@@ -0,0 +1,23 @@
+class Logger {
+  constructor(message) {
+    console.log("executed", message, new Date());
+  }
+}
+class UserTrack {
+  constructor() {
+    console.log("user tracking");
+  }
+}
+class Message {
+  constructor(message) {
+    this.message = message;
+    this.middleware = [new Logger(this.message), new UserTrack()];
+  }
+
+  send() {
+    console.log("message sent");
+  }
+}
+
+const m = new Message("Hi there");
+m.send();
